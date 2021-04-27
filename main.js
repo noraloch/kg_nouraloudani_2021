e3642e19748c27cf3f7644e49a3e38b56a46c86a
Noura <contact.nouraloudani@gmail.com>
2021-04-26 21:33:22 -0400

    Created a helper function that takes in a digit string and converts it to its equivalent spelling

---
+++ main.js
@@ -0,0 +1,38 @@
+//helper
+function numberConverter(x) {
+    let number = null;
+    switch (x) {
+        case "0":
+            number = "Zero";
+            break;
+        case "1":
+            number = "One";
+            break;
+        case "2":
+            number = "Two";
+            break;
+        case "3":
+            number = "Three";
+            break;
+        case "4":
+            number = "Four";
+            break;
+        case "5":
+            number = "Five";
+            break;
+        case "6":
+            number = "Six";
+            break;
+        case "7":
+            number = "Seven";
+            break;
+        case "8":
+            number = "Eight";
+            break;
+        case "9":
+            number = "Nine";
+            break;
+    }
+    return number
+}
+//end of helper
